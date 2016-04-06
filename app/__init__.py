4378ea11666bc4b871de243dfbb612ccf2e257b0
Justen Morgan <justenamorgan@gmail.com>
2016-04-06 14:55:30 +0000

    sends email

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:project2@localhost/projectdb"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qvufzfhjnrzrne:PWKGvd6x1FghXkr90I4MXo3kUG@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d4fgu381smcp9t"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:project2@localhost/projectdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qvufzfhjnrzrne:PWKGvd6x1FghXkr90I4MXo3kUG@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d4fgu381smcp9t"
 db = SQLAlchemy(app)
 db.create_all()
 
